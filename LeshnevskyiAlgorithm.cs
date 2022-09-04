b52521b8d8ea2ed95cafcecb73d6d431c862d555
leshnevskyi <leshnevskyi@gmail.com>
2022-09-05 02:12:43 +0300

    init: project scaffolding

---
+++ LeshnevskyiAlgorithm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using Robot.Common;
+
+namespace Leshnevskyi.Nazarii.RobotChallenge {
+	public class LeshnevskyiAlgorithm : IRobotAlgorithm {
+		public string Author => "Nazarii Leshnevskyi";
+
+		public RobotCommand DoStep(IList<Robot.Common.Robot> robots, int robotToMoveIndex, Map map) {
+			throw new NotImplementedException();
+		}
+	}
+}
